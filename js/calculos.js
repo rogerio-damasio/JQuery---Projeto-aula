3df8fa62007a1cf1c5e762c1de289fb49e24253f
João <joaopedrocv.ce@gmail.com>
2026-03-18 14:57:41 -0300

    first commit

--- js/calculos.js
+++ js/calculos.js
@@ -1,63 +1,100 @@
-$("#calcular").click(function () {
-    var valor1 = parseFloat($("#txtvalor1").val());
-    var valor2 = parseFloat($("#txtvalor2").val());
-    var valor3 = parseFloat($("#txtvalor3").val());
+$(document).ready(function () {
+    $("#calcular").click(function () {
+        let n1 = parseFloat($("#n1").val());
+        let n2 = parseFloat($("#n2").val());
+        let operacao = $("input[name='operacao']:checked").val();
+        let resultado = calcular(n1, n2, operacao);
+        
 
-    if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3)) {
-        alert("Por favor, insira nrs válidos!");
-        return;
-    }
+        $("#resultado").html("Resultado: " + resultado );
+        
+ if(resultado%2==0){
+    $("#txtmsg_cal").html("Par")
+} else {
+     $("#txtmsg_cal").html("Impar")
+};
 
-    var resultado = parseFloat((valor1 + valor2 + valor3).toFixed(2));
-
-    var moeda = resultado.toLocaleString('pt-BR', {
-        style: "currency",
-        currency: "BRL",
-        minimumFractionDigits: 2
+ if(resultado<0){
+    $("#txtmsg_neg").html("Negativo")
+} else {
+     $("#txtmsg_neg").html("Positivo")
+}
     });
-
-    $("#txtresultado")
-        .html("Valor Final " + resultado + "<br> Moeda " + moeda)
-        .css({
-            'font-weight': 'bold',
-            'font-size': '20px',
-            'color': '#0000ff'
-        });
-
-    if (resultado > 5) {
-        alert("Valor maior que 5");
-    } else if (resultado < 5) {
-        alert("Valor menor que 5");
-    } else {
-        alert("Igual a 5");
-    }
-
-    var mostrar = (resultado > 5) ? 'maior que 5' : 'menor ou igual a 5';
-
-    alert(mostrar);
-
-    switch (true) {
-        case (resultado > 10):
-            alert("Maior que 10");
-            break;
-    case (resultado >= 5):
-            alert("Maior que 5 e menor que 10");
-            break;
-        default:
-            alert("menor 5");
-            break;
-    }
-    
-    switch (parseInt(resultado)%2) {
-        case 0:
-            alert("par");
-            break;
-     case 1:
-            alert("impar");
-            break;
-        default:
-            alert("inválido");
-            break;
-    }
-
 });
+
+
+
+function limpar() {
+    $("#n1").val("");
+    $("#n1").focus();
+    $("#n2").val("");
+    $("input[name='operacao']").prop("checked", false);
+    $("#resultado").html("Resultado: ");
+    $("#txtmsg").html("Peso: ");
+    $("#txtmsg_cal").html("Impar ou Par: ");
+    $("#txtmsg_neg").html("Negativo ou Positivo: ");
+}
+
+function calcular(n1, n2, operacao) {
+    let resultado = 0;
+
+    if (operacao == "soma") {
+        resultado = n1 + n2;
+    } else if (operacao == "sub") {
+        resultado = n1 - n2;
+    } else if (operacao == "mult") {
+        resultado = n1 * n2;
+    } else if (operacao == "div") {
+        resultado = n1 / n2;
+    }
+
+    switch (operacao) {
+        case "soma":
+            resultado = n1 + n2;
+            break;
+        case "sub":
+            resultado = n1 - n2;
+            break;
+        case "mult":
+            resultado = n1 * n2;
+            break;
+        case "div":
+            resultado = n1 / n2;
+            break;
+    }
+
+    return resultado;
+
+}
+
+$("#calcular_imc").click(function (){
+        let n1 = parseFloat($("#n1").val());
+        let n2 = parseFloat($("#n2").val());
+        let resultado = n1/(n2*n2)
+        
+
+        $("#resultado").html("IMC: " + resultado );
+
+        if(resultado<18.5){
+    $("#txtmsg").html("Abaixo do peso")
+} else if (resultado>18.5 && resultado<24.9){
+     $("#txtmsg").html("Peso ideal")
+} else if (resultado>25 && resultado<29.9){
+     $("#txtmsg").html("Sobrepeso")
+} else if (resultado>30 && resultado<39.9){
+     $("#txtmsg").html("Obesidade")
+}else if (resultado>40){
+     $("#txtmsg").html("Obesidade mórbida")
+}else {
+     $("#txtmsg").html("ERRO")
+}
+});
+
+function limpar() {
+    $("#n1").val("");
+    $("#n1").focus();
+    $("#n2").val("");
+    $("#resultado").html("Resultado: ");
+    $("#txtmsg").html("IMC: ");
+}
+
